108c0cf1c63168b2d847a041f98bc0be9db24a8f
leenapark <hrn424@gmail.com>
2023-04-06 16:15:23 +0900

    230405 Training code-delete User post name:None

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,8 @@ class Post(models.Model):
   created_at = models.DateField(auto_now_add=True)
   updated_at = models.DateField(auto_now=True)
 
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  # author = models.ForeignKey(User, on_delete=models.CASCADE) User 삭제 시 포스트도 삭제 된다
+  author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
   def __str__(self):
     return f'[{self.pk}] {self.title} :: {self.author}'
