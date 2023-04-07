c78b99d4cf8a19238a72701feb97388fb65aafad
leenapark <hrn424@gmail.com>
2023-04-07 14:33:00 +0900

    230407 Training code - categoty module

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # /blog/
     path('', views.PostList.as_view(), name='post_list'),
     path('<int:pk>/', views.PostDetail.as_view(), name='post_etail'),
-
+    path("category/<str:slug>/", views.category_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 
@@ -11,6 +11,50 @@ class PostList(ListView):
   # template_name = 'blog/post_list.html'
   ordering = "-pk"
 
+  def get_context_data(self, **kwargs):
+    context = super(PostList, self).get_context_data()
+    context["categories"] = Category.objects.all()
+    context["no_category_post_count"] = Post.objects.filter(category = None).count()
+    return context
+
 class PostDetail(DetailView):
   model = Post
   # template_name = 'blog/post_detail.html'
+  
+  def get_context_data(self, **kwargs):
+    context = super(PostDetail, self).get_context_data()
+    context["categories"] = Category.objects.all()
+    context["no_category_post_count"] = Post.objects.filter(category = None).count()
+    return context
+  
+def category_page(request, slug):
+  # context = {}
+  # category = Category.objects.get(slug=slug)
+  # context["post_list"] = Post.objects.filter(category=category)
+  # context["categories"] = Category.objects.all()
+  # context["no_category_post_count"] = Post.objects.filter(category = None).count()
+  # context["category"] = category
+  # print(context)
+
+  # return render(
+  #   request,
+  #   "blog/post_list.html",
+  #   context
+  # )
+  if slug == "no_category":
+    category = "미분류"
+    post_list = Post.objects.filter(category=None)
+  else:
+    category = Category.objects.get(slug=slug)
+    post_list = Post.objects.filter(category=category)
+
+  return render(
+    request,
+    "blog/post_list.html",
+    {
+      "post_list": post_list,
+      "categories": Category.objects.all(),
+      "no_category_post_count": Post.objects.filter(category=None).count(),
+      "category": category
+    }
+  )
