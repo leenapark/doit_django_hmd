c0cb213b568af29358dae3cc922862107c14656f
leenapark <hrn424@gmail.com>
2023-04-07 17:59:35 +0900

    230407 Training code - categoty module, tag, write Form, update form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post, Category, Tag
 
 # Create your views here.
@@ -42,6 +43,19 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
       return super(PostCreate, self).form_valid(form)
     else:
       return redirect("/blog/")
+    
+# 포스트 수정
+class PostUpdate(LoginRequiredMixin, UpdateView):
+  model = Post
+  fields = ["title", "hook_text", "content", "head_image", "file_upload", "category", "tags"]
+
+  template_name = "blog/post_update_form.html"
+
+  def dispatch(self, request, *args, **kwargs):
+    if request.user.is_authenticated and request.user == self.get_object().author:
+      return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+    else:
+      raise PermissionDenied
   
 def category_page(request, slug):
   # context = {}
